d9d1fe17ce825c15ca2506d38967813d7d8c4a7a
achomonster <JoleroYT@gmail.com>
2023-09-24 17:56:52 +0200

    1st

---
+++ arbitrage/arbitrage.py
@@ -0,0 +1,85 @@
+import requests
+
+# Function to fetch the latest trade price from Binance
+def get_binance_latest_trade_price():
+    # Binance API endpoint for ticker price
+    url = 'https://api.binance.com/api/v3/ticker/price'
+    symbol = 'BTCUSDT'
+    params = {'symbol': symbol}
+
+    response = requests.get(url, params=params)
+
+    if response.status_code == 200:
+        ticker_data = response.json()
+        latest_trade_price = float(ticker_data['price'])
+        return latest_trade_price
+    else:
+        print('Failed to retrieve ticker price from Binance. Status code:', response.status_code)
+        return None
+
+# Function to fetch ticker data (last trade price) from Coinbase Pro
+def get_coinbase_pro_last_trade_price():
+    url = 'https://api.pro.coinbase.com/products/BTC-USD/ticker'
+
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        ticker_data = response.json()
+        current_price = float(ticker_data['price'])
+        return current_price
+    else:
+        print('Failed to retrieve ticker data from Coinbase Pro. Status code:', response.status_code)
+        return None
+
+# Function to fetch the latest trade price from gate.io
+def get_gateio_latest_trade_price():
+    # gate.io API endpoint for ticker price
+    url = 'https://api.gate.io/api2/1/ticker/btc_usdt'
+
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        ticker_data = response.json()
+        latest_trade_price = float(ticker_data['last'])
+        return latest_trade_price
+    else:
+        print('Failed to retrieve ticker price from gate.io. Status code:', response.status_code)
+        return None
+
+# Calculate and compare prices
+# Modify the function to return a list of opportunities
+def find_max_arbitrage_opportunity():
+    binance_price = get_binance_latest_trade_price()
+    coinbase_price = get_coinbase_pro_last_trade_price()
+    gateio_price = get_gateio_latest_trade_price()
+
+    opportunities = []  # Initialize an empty list to store opportunities
+
+    if binance_price is not None and coinbase_price is not None and gateio_price is not None:
+        price_difference_bc = binance_price - coinbase_price
+        price_difference_bg = binance_price - gateio_price
+        price_difference_cg = coinbase_price - gateio_price
+
+        max_difference = max(price_difference_bc, price_difference_bg, price_difference_cg)
+
+        if max_difference == price_difference_bc:
+            profit_percent = (abs(price_difference_bc) / coinbase_price) * 100
+            opportunities.append(f'Buy on Coinbase Pro, Sell on Binance. Profit: {abs(price_difference_bc):.2f} USD ({profit_percent:.2f}%)')
+        elif max_difference == price_difference_bg:
+            profit_percent = (abs(price_difference_bg) / gateio_price) * 100
+            opportunities.append(f'Buy on gate.io, Sell on Binance. Profit: {abs(price_difference_bg):.2f} USD ({profit_percent:.2f}%)')
+        else:
+            profit_percent = (abs(price_difference_cg) / gateio_price) * 100
+            opportunities.append(f'Buy on gate.io, Sell on Coinbase Pro. Profit: {abs(price_difference_cg):.2f} USD ({profit_percent:.2f}%)')
+    else:
+        opportunities.append('Price comparison failed due to missing data.')
+
+    return opportunities  # Return the list of opportunities
+
+
+# Call the function to find the biggest arbitrage opportunity
+def main():
+    find_max_arbitrage_opportunity()
+
+if __name__ == '__main__':
+    main()
